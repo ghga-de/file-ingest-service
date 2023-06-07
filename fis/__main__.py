c4b4d09f1161e367d1467817aa3e03bc34d997ef
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2023-06-07 09:43:37 +0000

    Small __main__ change

--- fis/__main__.py
+++ fis/__main__.py
@@ -14,19 +14,7 @@
 # limitations under the License.
 """Entrypoint of the package"""
 
-import asyncio
-
-from ghga_service_commons.api import run_server
-
-from .api.main import app  # noqa: F401 pylint: disable=unused-import
-from .config import CONFIG, Config
-
-
-def run(config: Config = CONFIG):
-    """Run the service"""
-    # Please adapt to package name
-    asyncio.run(run_server(app="fis.__main__:app", config=config))
-
+from fis.cli import cli
 
 if __name__ == "__main__":
-    run()
+    cli()
